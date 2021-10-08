9a15ab92ce938ab65f0e740e89b1affb6f2f565f
xuyaming0800 <xuaming0800@126.com>
2021-10-08 18:39:33 +0800

    修复安全组创建bug

--- ksyun/data_source_ksyun_network_interfaces.go
+++ ksyun/data_source_ksyun_network_interfaces.go
@@ -1,7 +1,6 @@
 package ksyun
 
 import (
-	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
@@ -74,7 +73,19 @@ func dataSourceKsyunNetworkInterfaces() *schema.Resource {
 				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
 						"network_interface_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"network_interface_name": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
@@ -129,56 +140,7 @@ func dataSourceKsyunNetworkInterfaces() *schema.Resource {
 	}
 }
 
-func dataSourceKsyunNetworkInterfacesRead(d *schema.ResourceData, m interface{}) error {
-	conn := m.(*KsyunClient).vpcconn
-	req := make(map[string]interface{})
-	var NetworkInterfaceIds []string
-	if ids, ok := d.GetOk("ids"); ok {
-		NetworkInterfaceIds = SchemaSetToStringSlice(ids)
-	}
-	for k, v := range NetworkInterfaceIds {
-		if v == "" {
-			continue
-		}
-		req[fmt.Sprintf("NetworkInterfaceId.%d", k+1)] = v
-	}
-	filters := []string{
-		"vpc_id",
-		"subnet_id",
-		"securitygroup_id",
-		"instance_type",
-		"instance_id",
-		"private_ip_address",
-	}
-	req = *SchemaSetsToFilterMap(d, filters, &req)
-	var allNetworkInterfaces []interface{}
-	resp, err := conn.DescribeNetworkInterfaces(&req)
-	if err != nil {
-		return fmt.Errorf("error on reading NetworkInterface list, %s", req)
-	}
-	itemSet, ok := (*resp)["NetworkInterfaceSet"]
-	if !ok {
-		return fmt.Errorf("error on reading NetworkInterface set")
-	}
-	items, ok := itemSet.([]interface{})
-	if !ok {
-		return nil
-	}
-	if items == nil || len(items) < 1 {
-		return nil
-	}
-	allNetworkInterfaces = append(allNetworkInterfaces, items...)
-	datas := GetSubSliceDByRep(allNetworkInterfaces, vpcNetworkInterfaceKeys)
-	for _, v := range datas {
-		securitygroup := v["security_group_set"]
-		if securityGroups, ok := securitygroup.([]interface{}); ok {
-			securitygroupSets := GetSubSliceDByRep(securityGroups, groupIdentifierKeys)
-			v["security_group_set"] = securitygroupSets
-		}
-	}
-	err = dataSourceKscSave(d, "network_interfaces", NetworkInterfaceIds, datas)
-	if err != nil {
-		return fmt.Errorf("error on save NetworkInterface list, %s", err)
-	}
-	return nil
+func dataSourceKsyunNetworkInterfacesRead(d *schema.ResourceData, meta interface{}) error {
+	vpcService := VpcService{meta.(*KsyunClient)}
+	return vpcService.ReadAndSetNetworkInterfaces(d, dataSourceKsyunNetworkInterfaces())
 }
