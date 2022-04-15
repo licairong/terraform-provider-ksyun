e32665d5d85e19c40d35276d262a151698190ffd
x6doooo <x6doooo@gmail.com>
2022-04-15 18:38:06 +0800

    update

--- example/subnet/main.tf
+++ example/subnet/main.tf
@@ -10,8 +10,6 @@ resource "ksyun_subnet" "test" {
   subnet_name      = "tf-acc-subnet1"
   	cidr_block = "10.0.5.0/24"
       subnet_type = "Normal"
-      dhcp_ip_from = "10.0.5.2"
-      dhcp_ip_to = "10.0.5.253"
       vpc_id  = "${ksyun_vpc.test.id}"
       gateway_ip = "10.0.5.1"
       dns1 = "198.18.254.41"
