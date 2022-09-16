122786b63a6d6da38ed24446c474f58dce4f2f5d
Dx. Yang <x6doooo@gmail.com>
2022-09-16 17:18:59 +0800

    Update main.tf

--- example/dataSSHKeys/main.tf
+++ example/dataSSHKeys/main.tf
@@ -5,8 +5,26 @@ provider "ksyun" {
 
 # Get  keys
 data "ksyun_ssh_keys" "default" {
+#  根据key的名称查找
+  key_name="ydx_zh_pub"
+
+#  查找多个名称
+#  key_names = ["ydx_zh_pub", "test"]
+
+# 根据key的id（指纹）查询
+#  ids = ["b433f65e-29e7-4b77-b40d-xxxxxxxxxxxxx", "7f5e9a85-9f89-4988-9792-xxxxxxxxxxxxx"]
+
   output_file="output_result"
-  ids=[]
-  key_name=""
 }
+
+# 返回值示例
+
+#[
+#  {
+#    "create_time": "2022-08-01 10:36:52",
+#    "key_id": "b433f65e-29e7-4b77-b40d-142xxxxxxxxx",
+#    "key_name": "test",
+#    "public_key": "ssh-rsa xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#  }
+# ]
 
